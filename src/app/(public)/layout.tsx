37c7c5fe7203a6fca56d8237170472f31729afc0
keelann95 <sephen.mwaniki@student.moringaschool.com>
2025-05-08 17:29:09 +0300

    adds the expected user props

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,10 +2,18 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
-export default function PublicLayout({ children }: { children: React.ReactNode }) {
+export default function PublicLayout({
+  children,
+  userRole,
+  userData,
+}: {
+  children: React.ReactNode;
+  userRole: any;
+  userData: any;
+}) {
   return (
     <>
-      <Header />
+      <Header userRole={userRole} userData={userData} />
       <main className="pt-16">{children}</main>
       <Footer />
     </>
